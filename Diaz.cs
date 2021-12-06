fdb306365380e30ed807b54a536c1b56e06445c2
Milton Eduardo Diaz Morcillo <mediazm@pucese.edu.ec>
2021-12-06 17:13:38 -0500

    Formulario Milton Diaz

---
+++ Diaz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFormApellido
+{
+    public partial class Diaz : Form
+    {
+        public Diaz()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saludos y bienvenidos de parte de Milton ....  :3 ");
+        }
+    }
+}
