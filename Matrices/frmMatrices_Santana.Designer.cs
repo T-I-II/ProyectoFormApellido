dc3eb750c6993fd6f22f104b79cf37e273eb0f9c
lenadroi35s <santanaleandro099@gmail.com>
2021-12-07 13:10:08 -0500

    Santana

---
+++ Matrices/frmMatrices_Santana.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace ProyectoFormApellido.Matrices
+{
+    partial class frmMatrices_Santana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_M1 = new System.Windows.Forms.Button();
+            this.btn_M2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_F1_M1 = new System.Windows.Forms.TextBox();
+            this.txt_C1_M1 = new System.Windows.Forms.TextBox();
+            this.txt_F2_M2 = new System.Windows.Forms.TextBox();
+            this.txt_C2_M2 = new System.Windows.Forms.TextBox();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_F1_M1);
+            this.groupBox1.Controls.Add(this.btn_M1);
+            this.groupBox1.Controls.Add(this.txt_C1_M1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Matriz #1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_M2);
+            this.groupBox2.Controls.Add(this.txt_C2_M2);
+            this.groupBox2.Controls.Add(this.txt_F2_M2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(369, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(368, 255);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Matriz #2";
+            // 
+            // btn_M1
+            // 
+            this.btn_M1.Location = new System.Drawing.Point(165, 170);
+            this.btn_M1.Name = "btn_M1";
+            this.btn_M1.Size = new System.Drawing.Size(112, 47);
+            this.btn_M1.TabIndex = 2;
+            this.btn_M1.Text = "Crear matriz #1";
+            this.btn_M1.UseVisualStyleBackColor = true;
+            this.btn_M1.Click += new System.EventHandler(this.btn_M1_Click);
+            // 
+            // btn_M2
+            // 
+            this.btn_M2.Location = new System.Drawing.Point(235, 164);
+            this.btn_M2.Name = "btn_M2";
+            this.btn_M2.Size = new System.Drawing.Size(112, 47);
+            this.btn_M2.TabIndex = 3;
+            this.btn_M2.Text = "Crear matriz #2";
+            this.btn_M2.UseVisualStyleBackColor = true;
+            this.btn_M2.Click += new System.EventHandler(this.btn_M2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Columnas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Filas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Columnas";
+            // 
+            // txt_F1_M1
+            // 
+            this.txt_F1_M1.Location = new System.Drawing.Point(78, 40);
+            this.txt_F1_M1.Name = "txt_F1_M1";
+            this.txt_F1_M1.Size = new System.Drawing.Size(144, 20);
+            this.txt_F1_M1.TabIndex = 2;
+            // 
+            // txt_C1_M1
+            // 
+            this.txt_C1_M1.Location = new System.Drawing.Point(78, 132);
+            this.txt_C1_M1.Name = "txt_C1_M1";
+            this.txt_C1_M1.Size = new System.Drawing.Size(144, 20);
+            this.txt_C1_M1.TabIndex = 4;
+            // 
+            // txt_F2_M2
+            // 
+            this.txt_F2_M2.Location = new System.Drawing.Point(90, 37);
+            this.txt_F2_M2.Name = "txt_F2_M2";
+            this.txt_F2_M2.Size = new System.Drawing.Size(144, 20);
+            this.txt_F2_M2.TabIndex = 5;
+            // 
+            // txt_C2_M2
+            // 
+            this.txt_C2_M2.Location = new System.Drawing.Point(91, 132);
+            this.txt_C2_M2.Name = "txt_C2_M2";
+            this.txt_C2_M2.Size = new System.Drawing.Size(144, 20);
+            this.txt_C2_M2.TabIndex = 5;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(305, 252);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(107, 65);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "Mulitiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // frmMatrices_Santana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::ProyectoFormApellido.Properties.Resources.wallpaper_full_hd_1080p_1920x1080;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMatrices_Santana";
+            this.Text = "Multiplicacion de matrices";
+            this.Load += new System.EventHandler(this.frmMatrices_Santana_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_F1_M1;
+        private System.Windows.Forms.Button btn_M1;
+        private System.Windows.Forms.TextBox txt_C1_M1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_M2;
+        private System.Windows.Forms.TextBox txt_C2_M2;
+        private System.Windows.Forms.TextBox txt_F2_M2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMultiplicar;
+    }
+}
---
+++ Matrices/frmMatrices_Santana.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFormApellido.Matrices
+{
+    public partial class frmMatrices_Santana : Form
+    {
+        TextBox[] matriz1 = new TextBox[50];
+        TextBox[] matriz2 = new TextBox[50];
+        public frmMatrices_Santana()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMatrices_Santana_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_M1_Click(object sender, EventArgs e)
+        {
+            //crear controles de tiempo de ejecucion 
+            
+            int filas = int.Parse(this.txt_F1_M1.Text);
+            int columnas = int.Parse(this.txt_C1_M1.Text);
+            int i = 30;
+            int y = 300;
+            for (int x = 0; x < (filas*columnas); x++)
+            {
+                matriz1[x] = new TextBox();
+                matriz1[x].Text = "0";
+                matriz1[x].Width = 40; //ancho
+                matriz1[x].Location = new Point(i, y);//posicion 
+                this.Controls.Add(matriz1[x]);//mostrar
+                i += 42;
+                
+                if((x+1) % columnas == 0)
+                {
+                    y += 30;
+                    i = 30;
+                }
+            }
+            MessageBox.Show("Matriz creada");
+        }
+
+        private void btn_M2_Click(object sender, EventArgs e)
+        {
+            //crear controles de tiempo de ejecucion 
+            TextBox[] matriz2 = new TextBox[50];
+            int filas = int.Parse(this.txt_F2_M2.Text);
+            int columnas = int.Parse(this.txt_C2_M2.Text);
+            int i = 380;
+            int y = 300;
+            for (int x = 0; x < (filas * columnas); x++)
+            {
+                matriz2[x] = new TextBox();
+                matriz2[x].Text = "0";
+                matriz2[x].Width = 40; //ancho
+                matriz2[x].Location = new Point(i, y);//posicion 
+                this.Controls.Add(matriz2[x]);//mostrar
+                i += 42;
+
+                if ((x + 1) % columnas == 0)
+                {
+                    y += 30;
+                    i = 380;
+                }
+            }
+            MessageBox.Show("Matriz creada");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int[][] m1 = new int[3][];
+            m1[0] = new int [3];
+            m1[1] = new int[3];
+            m1[2] = new int[3];
+
+            //pasa los elementos del matriz1 a la matriz de los enteros m1
+            int columas = int.Parse(this.txt_C1_M1.Text);
+            int i = 0, j = 0;
+            for(int x1=0; x1<9; x1++)
+            {
+                m1[i][j] = int.Parse(matriz1[x1].Text);
+                j++;
+                if((x1 + 1) % columas == 0)
+                {
+                    i++;
+                    j = 0;
+                }
+                
+
+            }
+            //imprimiar
+            for(i = 0; i<3; i ++)
+            {
+                for (j = 0; j < 3; j++){
+                Console.Write($"{m1[i][j]}");
+            }
+            Console.WriteLine();
+            }
+        }
+    }
+}
