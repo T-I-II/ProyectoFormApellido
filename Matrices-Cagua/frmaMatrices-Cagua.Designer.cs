600d954de71ded818726dc4117c1edc2456057d1
ChristopherCagua <caguachristopher@gmail.com>
2021-12-07 13:19:18 -0500

    Nueva rama de cagua cambio de matrices

---
+++ Matrices-Cagua/frmaMatrices-Cagua.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace ProyectoFormApellido.Matrices_Cagua
+{
+    partial class frmaMatrices_Cagua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFilas1 = new System.Windows.Forms.TextBox();
+            this.txtCols1 = new System.Windows.Forms.TextBox();
+            this.txtFilas2 = new System.Windows.Forms.TextBox();
+            this.txtCols2 = new System.Windows.Forms.TextBox();
+            this.btnMatriz1 = new System.Windows.Forms.Button();
+            this.btnMatriz2 = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMatriz1);
+            this.groupBox1.Controls.Add(this.txtCols1);
+            this.groupBox1.Controls.Add(this.txtFilas1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(35, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 205);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Matriz#2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnMatriz2);
+            this.groupBox2.Controls.Add(this.txtCols2);
+            this.groupBox2.Controls.Add(this.txtFilas2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(411, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 205);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Matriz#2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Columnas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Filas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Columnas";
+            // 
+            // txtFilas1
+            // 
+            this.txtFilas1.Location = new System.Drawing.Point(104, 31);
+            this.txtFilas1.Name = "txtFilas1";
+            this.txtFilas1.Size = new System.Drawing.Size(100, 20);
+            this.txtFilas1.TabIndex = 1;
+            // 
+            // txtCols1
+            // 
+            this.txtCols1.Location = new System.Drawing.Point(104, 90);
+            this.txtCols1.Name = "txtCols1";
+            this.txtCols1.Size = new System.Drawing.Size(100, 20);
+            this.txtCols1.TabIndex = 2;
+            // 
+            // txtFilas2
+            // 
+            this.txtFilas2.Location = new System.Drawing.Point(94, 20);
+            this.txtFilas2.Name = "txtFilas2";
+            this.txtFilas2.Size = new System.Drawing.Size(100, 20);
+            this.txtFilas2.TabIndex = 2;
+            // 
+            // txtCols2
+            // 
+            this.txtCols2.Location = new System.Drawing.Point(94, 70);
+            this.txtCols2.Name = "txtCols2";
+            this.txtCols2.Size = new System.Drawing.Size(100, 20);
+            this.txtCols2.TabIndex = 3;
+            // 
+            // btnMatriz1
+            // 
+            this.btnMatriz1.Location = new System.Drawing.Point(44, 146);
+            this.btnMatriz1.Name = "btnMatriz1";
+            this.btnMatriz1.Size = new System.Drawing.Size(129, 33);
+            this.btnMatriz1.TabIndex = 3;
+            this.btnMatriz1.Text = "Crear Matriz #1";
+            this.btnMatriz1.UseVisualStyleBackColor = true;
+            this.btnMatriz1.Click += new System.EventHandler(this.btnMatriz1_Click);
+            // 
+            // btnMatriz2
+            // 
+            this.btnMatriz2.Location = new System.Drawing.Point(49, 146);
+            this.btnMatriz2.Name = "btnMatriz2";
+            this.btnMatriz2.Size = new System.Drawing.Size(145, 33);
+            this.btnMatriz2.TabIndex = 4;
+            this.btnMatriz2.Text = "Crear Matriz #2";
+            this.btnMatriz2.UseVisualStyleBackColor = true;
+            this.btnMatriz2.Click += new System.EventHandler(this.btnMatriz2_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(260, 260);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(165, 46);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "Multiplicar matrices";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // frmaMatrices_Cagua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 547);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmaMatrices_Cagua";
+            this.Text = "frmaMatrices_Cagua";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMatriz1;
+        private System.Windows.Forms.TextBox txtCols1;
+        private System.Windows.Forms.TextBox txtFilas1;
+        private System.Windows.Forms.Button btnMatriz2;
+        private System.Windows.Forms.TextBox txtCols2;
+        private System.Windows.Forms.TextBox txtFilas2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnMultiplicar;
+    }
+}
---
+++ Matrices-Cagua/frmaMatrices-Cagua.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFormApellido.Matrices_Cagua
+{
+    public partial class frmaMatrices_Cagua : Form
+    {
+        TextBox[] matriz1 = new TextBox[50];
+        public frmaMatrices_Cagua()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMatriz2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMatriz1_Click(object sender, EventArgs e)
+        {
+            //creacion de controles en tiempo de ejecucion
+            
+            int filas = int.Parse(this.txtFilas1.Text);
+            int cols = int.Parse(this.txtCols1.Text);
+            int x = 35, y = 250;
+            for(int i=0; i<9; i++)
+            {
+                matriz1[0] = new TextBox();
+                matriz1[0].Text = "0";
+                matriz1[0].Width = 40; //ancho del control
+                matriz1[0].Location = new Point(x, y);//posicion del control
+                this.Controls.Add(matriz1[0]);//mostrar el control
+                x += 42;
+                if ((i+1)% cols ==0)
+                {
+                    y += 30;
+                    x -= 126;
+                }
+
+            }
+
+            
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            //se suma unamatriz de 3x3
+            int[][] m1 = new int[3][];
+            m1[0] = new int[3];
+            m1[1] = new int[3];
+            m1[2] = new int[3];
+            int i = 0, j = 0;
+            for(int k=0;  k<9; k++)
+            {
+                m1[i][j] = int.Parse(matriz1[k].Text);
+                j++;
+                if ((k+1)% Cols1 ==0)
+                {
+                    i++;
+                    j = 0;
+                }
+            }
+
+            //imprime la matriz m1
+            for(i=0; i<3; i++)
+            {
+                for(j=0; j<3; j++)
+                {
+                    Console.Write($"{m1[i][j]}");
+                }
+                Console.WriteLine();            }
+        }
+    }
+}
