4391922166a5e316843f7102cc2c9c08457641f6
Milton Eduardo Diaz Morcillo <mediazm@pucese.edu.ec>
2021-12-12 15:22:53 -0500

    Diazm

--- matrices/FrnDiaz.Designer.cs
+++ matrices/FrnDiaz.Designer.cs
@@ -30,18 +30,27 @@ namespace ProyectoFormApellido.matrices
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtFilas1 = new System.Windows.Forms.TextBox();
+            this.btnMatriz1 = new System.Windows.Forms.Button();
             this.txtcols1 = new System.Windows.Forms.TextBox();
+            this.txtFilas1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnMatriz2 = new System.Windows.Forms.Button();
             this.txtFilas2 = new System.Windows.Forms.TextBox();
             this.txtcls2 = new System.Windows.Forms.TextBox();
-            this.btnMatriz1 = new System.Windows.Forms.Button();
-            this.btnMatriz2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -53,12 +62,63 @@ namespace ProyectoFormApellido.matrices
             this.groupBox1.Controls.Add(this.txtFilas1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Location = new System.Drawing.Point(16, 33);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(352, 253);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(469, 311);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Matriz #1";
+            // 
+            // btnMatriz1
+            // 
+            this.btnMatriz1.Location = new System.Drawing.Point(41, 214);
+            this.btnMatriz1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMatriz1.Name = "btnMatriz1";
+            this.btnMatriz1.Size = new System.Drawing.Size(341, 44);
+            this.btnMatriz1.TabIndex = 4;
+            this.btnMatriz1.Text = "Crear matriz #1";
+            this.btnMatriz1.UseVisualStyleBackColor = true;
+            this.btnMatriz1.Click += new System.EventHandler(this.btnMatriz1_Click);
+            // 
+            // txtcols1
+            // 
+            this.txtcols1.Location = new System.Drawing.Point(145, 119);
+            this.txtcols1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcols1.Name = "txtcols1";
+            this.txtcols1.Size = new System.Drawing.Size(132, 22);
+            this.txtcols1.TabIndex = 3;
+            this.txtcols1.Text = "3";
+            // 
+            // txtFilas1
+            // 
+            this.txtFilas1.Location = new System.Drawing.Point(145, 42);
+            this.txtFilas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFilas1.Name = "txtFilas1";
+            this.txtFilas1.Size = new System.Drawing.Size(132, 22);
+            this.txtFilas1.TabIndex = 2;
+            this.txtFilas1.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 119);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Columna";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fila";
             // 
             // groupBox2
             // 
@@ -67,117 +127,154 @@ namespace ProyectoFormApellido.matrices
             this.groupBox2.Controls.Add(this.txtcls2);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(409, 27);
+            this.groupBox2.Location = new System.Drawing.Point(545, 33);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(352, 253);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(469, 311);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Matriz #2";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(23, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Fila";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Fila";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Columna";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 97);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Columna";
-            // 
-            // txtFilas1
-            // 
-            this.txtFilas1.Location = new System.Drawing.Point(109, 34);
-            this.txtFilas1.Name = "txtFilas1";
-            this.txtFilas1.Size = new System.Drawing.Size(100, 20);
-            this.txtFilas1.TabIndex = 2;
-            this.txtFilas1.Text = "3";
-            // 
-            // txtcols1
-            // 
-            this.txtcols1.Location = new System.Drawing.Point(109, 97);
-            this.txtcols1.Name = "txtcols1";
-            this.txtcols1.Size = new System.Drawing.Size(100, 20);
-            this.txtcols1.TabIndex = 3;
-            this.txtcols1.Text = "3";
-            // 
-            // txtFilas2
-            // 
-            this.txtFilas2.Location = new System.Drawing.Point(118, 31);
-            this.txtFilas2.Name = "txtFilas2";
-            this.txtFilas2.Size = new System.Drawing.Size(100, 20);
-            this.txtFilas2.TabIndex = 4;
-            // 
-            // txtcls2
-            // 
-            this.txtcls2.Location = new System.Drawing.Point(118, 97);
-            this.txtcls2.Name = "txtcls2";
-            this.txtcls2.Size = new System.Drawing.Size(100, 20);
-            this.txtcls2.TabIndex = 5;
-            // 
-            // btnMatriz1
-            // 
-            this.btnMatriz1.Location = new System.Drawing.Point(31, 174);
-            this.btnMatriz1.Name = "btnMatriz1";
-            this.btnMatriz1.Size = new System.Drawing.Size(256, 36);
-            this.btnMatriz1.TabIndex = 4;
-            this.btnMatriz1.Text = "Crear matriz #1";
-            this.btnMatriz1.UseVisualStyleBackColor = true;
-            this.btnMatriz1.Click += new System.EventHandler(this.btnMatriz1_Click);
-            // 
             // btnMatriz2
             // 
-            this.btnMatriz2.Location = new System.Drawing.Point(51, 174);
+            this.btnMatriz2.Location = new System.Drawing.Point(68, 214);
+            this.btnMatriz2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMatriz2.Name = "btnMatriz2";
-            this.btnMatriz2.Size = new System.Drawing.Size(256, 36);
+            this.btnMatriz2.Size = new System.Drawing.Size(341, 44);
             this.btnMatriz2.TabIndex = 5;
             this.btnMatriz2.Text = "Crear matriz #2";
             this.btnMatriz2.UseVisualStyleBackColor = true;
             this.btnMatriz2.Click += new System.EventHandler(this.btnMatriz2_Click);
             // 
+            // txtFilas2
+            // 
+            this.txtFilas2.Location = new System.Drawing.Point(157, 38);
+            this.txtFilas2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFilas2.Name = "txtFilas2";
+            this.txtFilas2.Size = new System.Drawing.Size(132, 22);
+            this.txtFilas2.TabIndex = 4;
+            // 
+            // txtcls2
+            // 
+            this.txtcls2.Location = new System.Drawing.Point(157, 119);
+            this.txtcls2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcls2.Name = "txtcls2";
+            this.txtcls2.Size = new System.Drawing.Size(132, 22);
+            this.txtcls2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 119);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Columna";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fila";
+            // 
             // btnMultiplicar
             // 
-            this.btnMultiplicar.Location = new System.Drawing.Point(259, 305);
+            this.btnMultiplicar.Location = new System.Drawing.Point(478, 274);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMultiplicar.Name = "btnMultiplicar";
-            this.btnMultiplicar.Size = new System.Drawing.Size(242, 42);
+            this.btnMultiplicar.Size = new System.Drawing.Size(68, 62);
             this.btnMultiplicar.TabIndex = 2;
             this.btnMultiplicar.Text = "Multiplicador";
             this.btnMultiplicar.UseVisualStyleBackColor = true;
             this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(685, 365);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(808, 365);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(941, 365);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(685, 418);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(808, 418);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(941, 418);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(685, 473);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 8;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(808, 473);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 9;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(941, 473);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 10;
+            // 
             // FrnDiaz
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrnDiaz";
             this.Text = "FrnDiaz";
             this.groupBox1.ResumeLayout(false);
@@ -185,6 +282,7 @@ namespace ProyectoFormApellido.matrices
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -203,5 +301,14 @@ namespace ProyectoFormApellido.matrices
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
--- matrices/FrnDiaz.cs
+++ matrices/FrnDiaz.cs
@@ -74,7 +74,12 @@ namespace ProyectoFormApellido.matrices
             m1[0] = new int[3];
             m1[1] = new int[3];
             m1[2] = new int[3];
+            int[][] m2 = new int[3][];
+            m2[0] = new int[3];
+            m2[1] = new int[3];
+            m2[2] = new int[3];
             int columnas = int.Parse(this.txtcols1.Text);
+            int columnas2 = int.Parse(this.txtcls2.Text);
             int i = 0, j = 0;
             //HOLA
             for(int k=0; k<9; k++)
@@ -95,6 +100,45 @@ namespace ProyectoFormApellido.matrices
                 }
                 Console.WriteLine();
             }
+            int a = 0, b = 0;
+            for (int l = 0; l < 9; l++)
+            {
+                m2[a][b] = int.Parse(matriz2[l].Text);
+                b++;
+                if ((l + 1) % columnas2 == 0)
+                {
+                    a++;
+                    b = 0;
+                }
+            }
+            for (a = 0; a < 3; a++)
+            {
+                for (b = 0; b < 3; b++)
+                {
+                    Console.Write($"{m2[0][0]}");
+                }
+                Console.WriteLine();
+            }
+            Console.Write($"{m1[0][0] * m2[0][0]}");
+            int resul1 = (m1[0][0] * m2[0][0]) + (m1[0][01] * m2[1][0]) + (m1[0][02] * m2[2][0]);
+            this.textBox1.Text = resul1.ToString();
+            int resul2 = (m1[0][0] * m2[0][1]) + (m1[0][01] * m2[01][1]) + (m1[0][02] * m2[2][1]);
+            this.textBox2.Text = resul2.ToString();
+            int resul3 = (m1[0][0] * m2[0][2]) + (m1[0][1] * m2[01][2]) + (m1[0][2] * m2[02][2]);
+            this.textBox3.Text = resul3.ToString();
+            int resul4 = (m1[01][00] * m2[0][0]) + (m1[01][01] * m2[01][0]) + (m1[01][02] * m2[02][0]);
+            this.textBox4.Text = resul4.ToString();
+            int resul5 = (m1[01][00] * m2[0][1]) + (m1[01][01] * m2[01][1]) + (m1[01][02] * m2[02][1]);
+            this.textBox5.Text = resul5.ToString();
+            int resul6 = (m1[01][00] * m2[0][2]) + (m1[01][01] * m2[01][2]) + (m1[01][02] * m2[02][2]);
+            this.textBox6.Text = resul6.ToString();
+            int resul7 = (m1[02][00] * m2[0][0]) + (m1[02][01] * m2[01][0]) + (m1[02][02] * m2[02][0]);
+            this.textBox7.Text = resul7.ToString();
+            int resul8 = (m1[02][00] * m2[0][1]) + (m1[02][01] * m2[01][1]) + (m1[02][02] * m2[02][1]);
+            this.textBox8.Text = resul8.ToString();
+            int resul9 = (m1[02][00] * m2[0][2]) + (m1[02][01] * m2[01][2]) + (m1[02][02] * m2[02][2]);
+            this.textBox9.Text = resul9.ToString();
+
         }
     }
 }
